8b835d71701891afc8713d0bf52f6c46e02f6a13
Treasure Onoja <treasureonoja@Treasures-MacBook-Air.local>
2021-10-15 10:05:42 +0100

    First commit

---
+++ src/test/java/com/treasureio/line/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treasureio.line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
