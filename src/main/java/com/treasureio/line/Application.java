7f4138eadf40e28a81f1789d300dff3200aefb4c
Treasure Onoja <treasureonoja@Treasures-MacBook-Air.local>
2021-10-18 19:25:05 +0100

    Adding home controller

--- src/main/java/com/treasureio/line/Application.java
+++ src/main/java/com/treasureio/line/Application.java
@@ -4,12 +4,22 @@ import com.treasureio.line.models.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
+@Controller
 public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
